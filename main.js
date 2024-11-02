75f76e03e993aaa450130f6e14838d9919389178
Madina <madinash2304@gmail.com>
2024-11-02 17:14:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let all = prompt ('введите кол-во бананов')
+for(let i = 1; i <= all; i++) {
+    if(i===1 ){
+        console.log(i + ' banana');
+    } else{
+        console.log(i+ ' bananas');
+    }
+}
